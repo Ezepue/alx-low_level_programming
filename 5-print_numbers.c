01d40b9618a8532906dbe48f41f2fc226ea26fb4
Osmanthus07 <ebukaezepue@gmail.com>
2023-08-11 06:14:52 -0700

    fix

---
+++ 5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints_Numbers
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print_Tebahpla
+ * Return: Return 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
