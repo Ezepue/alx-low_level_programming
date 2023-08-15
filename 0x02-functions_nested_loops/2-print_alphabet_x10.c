5a81e0a31ad24b13b3d8db0d051ff04733db4e94
Osmanthus07 <ebukaezepue@gmail.com>
2023-08-15 09:04:21 -0700

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,20 +6,20 @@
  */
 void print_alphabet_x10(void)
 {
-    char letter;
-    int repetition;
+	char letter;
+	int repetition;
 
-    repetition = 0;
+	repetition = 0;
 
-    while (repetition < 10)
-    {
-        letter = 'a';
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-        repetition++;
-    }
+	while (repetition < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		repetition++;
+	}
 }
