14437a3d7a13141ac9d6e52287b6ba33ec693a12
Ezepue <ebukaezepue@gmail.com>
2023-11-13 16:32:55 +0100

    V3.1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Werror -pedatic -c -fPIC *.c
-gcc -shared -o liball *.o
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:LD_LIBRARY_PATH
